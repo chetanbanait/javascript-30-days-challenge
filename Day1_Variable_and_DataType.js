4298855a710a522beb2e136d7d45d46fd5d322a2
CHETAN BANAIT <121280853+chetanbanait@users.noreply.github.com>
2024-07-13 20:07:22 +0530

    Add files via upload

---
+++ Day1_Variable_and_DataType.js
@@ -0,0 +1,17 @@
+var num =23;
+console.log(num);
+
+let string = "chetan"
+console.log(string);
+
+const boolean = false
+console.log(boolean);
+
+let number = 45879
+console.log(typeof(number));
+
+let character ="chetan"
+console.log(typeof(character));
+
+let array = ['as','dg','gf', 'bf']
+console.log(Array.isArray);
