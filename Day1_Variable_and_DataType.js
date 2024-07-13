5ad5ab6cd491829aa38384982305493af31d477d
CHETAN BANAIT <121280853+chetanbanait@users.noreply.github.com>
2024-07-13 20:37:21 +0530

    Update Day1_Variable_and_DataType.js

--- Day1_Variable_and_DataType.js
+++ Day1_Variable_and_DataType.js
@@ -15,3 +15,24 @@ console.log(typeof(character));
 
 let array = ['as','dg','gf', 'bf']
 console.log(Array.isArray);
+
+let characterName = "John Doe";
+let characterAge = 35;
+let isOnScreen = true;
+
+console.log("Character Name:", characterName, "- Type:", typeof characterName);
+console.log("Character Age:", characterAge, "- Type:", typeof characterAge);
+console.log("Is On Screen:", isOnScreen, "- Type:", typeof isOnScreen);
+
+// Using let
+let x = 5;
+console.log(x); // Output: 5
+
+x = 10;
+console.log(x); // Output: 10
+
+// Using const
+const y = 5;
+console.log(y); // Output: 5
+
+y = 10; // Uncaught TypeError: Assignment to constant variable.
