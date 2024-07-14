3d8a068d10c22aed0e225a435f9fd2eba8212888
CHETAN BANAIT <121280853+chetanbanait@users.noreply.github.com>
2024-07-14 14:16:17 +0530

    Operator

---
+++ Day2_Operators_in_Javascript.js
@@ -0,0 +1,83 @@
+//Addition task 1
+let  a = 75;
+let b = 48;
+let c = a + b;
+console.log(c);
+
+//substraction task 2
+let d = 1752;
+let e = 4545;
+console.log(d - e);
+
+//multiplication task 3
+let f = 2245;
+let g = 4335;
+console.log(f * g);
+
+//divide task 4
+let h = 415;
+let i = 454;
+console.log(h / i);
+
+//remainder task 5
+let j = 455;
+let k = 78;
+console.log(j % k);
+
+//assignment operator task 6
+let l = 545;
+let m = 320;
+l = l += m;
+console.log(l);
+
+//assignment operator task 7
+let v = 556;
+let w = 445;
+v = v -= w;
+console.log(v);
+
+//comparison operator task 8 (>)
+let x = 4225;
+let y = 540;
+console.log(x < y);
+
+//comparison operator task 8 (<)
+let n = 4905;
+let o = 9459;
+console.log(n > o);
+
+//comparison operator task 9 (>=)
+let ss = 9742;
+let tt = 1040;
+console.log(ss >= tt);
+
+//comparison operator task 9 (<=)
+let nn = 2453;
+let oo = 2212;
+console.log(nn <= oo);
+
+//comparison operator task 10 (== , === )
+let kk = 52145;
+let ll = '5125';
+console.log(kk == ll);
+console.log(kk === ll);
+
+//logical operator task 11 (&&)
+
+let jj = 4241;
+let ke = 4754;
+console.log(jj == ke && ke == ll);
+
+//logical operator task 12 (||)
+console.log(jj == ke || ke == ll);
+
+//logical operator task 13 (!)
+let yy = true;
+let zz = false;
+console.log(!yy);
+console.log(!zz);
+
+
+//ternary operator task 14
+let i1 = 125;
+i1 === 125 ? console.log('positive') : console.log('negative');
